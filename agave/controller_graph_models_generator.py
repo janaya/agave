b3c441d3db199c551051bc2d9cb2ea3a216d2e74
Julia Anaya <julia.anaya@gmail.com>
2010-11-26 20:49:51 +0100

    Added db to all functions even if not being used

--- agave/controller_graph_models_generator.py
+++ agave/controller_graph_models_generator.py
@@ -58,10 +58,10 @@ def delete_CI_from_I(p, db='default'):
 ##############################################################################
 def create_AAp_from_I_A(a, p, db="default"):
     for a_to in p.actors.using(db).exclude(id=a.id):
-        if not AAp.objects.filter(actor_to=a,
+        if not AAp.objects.using(db).filter(actor_to=a,
                                   actor_from=a_to,
                                   instance=p):
-            aap, created = AAp.objects.get_or_create(actor_from=a,
+            aap, created = AAp.objects.using(db).get_or_create(actor_from=a,
                                                      actor_to=a_to,
                                                      instance=p)
             if created: logging.debug("Created AAp: " + aap.__unicode__())
@@ -88,10 +88,10 @@ def delete_AAp_from_I_A(p, a, db='default'):
 
 def create_CCp_from_I(c, p, db='default'):
     for c_to in p.concepts.using(db).exclude(id=c.id):
-        if not CCp.objects.filter(concept_from=c_to,
+        if not CCp.objects.using(db).filter(concept_from=c_to,
                                   concept_to=c,
                                   instance=p):
-            ccp, created = CCp.objects.get_or_create(concept_from=c,
+            ccp, created = CCp.objects.using(db).get_or_create(concept_from=c,
                                                      concept_to=c_to,
                                                      instance=p)
             if created: logging.debug("Created CCp: " + ccp.__unicode__())
@@ -154,7 +154,7 @@ def create_AC_from_C_I(c, p, db='default'):
 def delete_AC_from_I(p, db='default'):
 #    for a in p.actors.all():     
 #        for c in p.concepts.all():
-#            ac = ActorConcept.objects.get(actor=a,concept=c)
+#            ac = ActorConcept.objects.using(db).get(actor=a,concept=c)
     actors = p.actors.using(db).all()
     concepts = p.concepts.using(db).all()
     for ac in ActorConcept.objects.using(db).get(actor__in=actors,
@@ -426,116 +426,116 @@ def delete_AAc_from_I_C(p, c, db='default'):
 
 #def generate_AAp(pxds):
 #    AAp = set()
-#    for p in Instance.objects.filter(datasets=pxds):
+#    for p in Instance.objects.using(db).filter(datasets=pxds):
 #        for aa in combinations(p.actors.filter(datasets = pxds),2):
 #            AAp.add(aa)
 #    return AAp
 
-def generate_AAp(p):
-    [AAp.objects.get_or_create(actor_from=aa[0], actor_to=aa[1], instance=p) for aa in combinations(p.actors.all(), 2)]
+def generate_AAp(p, db='default'):
+    [AAp.objects.using(db).get_or_create(actor_from=aa[0], actor_to=aa[1], instance=p) for aa in combinations(p.actors.all(), 2)]
 
 #def generate_CCp(pxds):
 #    CCp = set()
-#    for p in Instance.objects.filter(datasets=pxds):
+#    for p in Instance.objects.using(db).filter(datasets=pxds):
 #        for cc in combinations(p.concepts.filter(datasets = pxds),2):
 #            CCp.add(cc)
 #    return CCp
 
 
-def generate_CCa(p):
+def generate_CCa(p, db='default'):
     for a in p.actors.all():
         for concept_from, concept_to in combinations(p.concepts.all(), 2):
-            CCa.objects.get_or_create(concept_from=concept_from, concept_to=concept_to, actor=a)
+            CCa.objects.using(db).get_or_create(concept_from=concept_from, concept_to=concept_to, actor=a)
 
-def generate_AAc(p):
+def generate_AAc(p, db='default'):
     for concept in p.concepts.all():
         for a_from, a_to in combinations(p.actors.all(), 2):
-            AAc.objects.get_or_create(actor_from=a_from, actor_to=a_to, concept=concept)
+            AAc.objects.using(db).get_or_create(actor_from=a_from, actor_to=a_to, concept=concept)
 
-def generate_CCbb():
-    for concept_to_id in CCb.objects.values('concept_to').distinct() :
-#        for ccb.concept_from in CCb.objects.filter(concept_to__id = concept_to__id):
-        concept = Concept.objects.get(id=concept_to_id)
+def generate_CCbb(db='default'):
+    for concept_to_id in CCb.objects.using(db).values('concept_to').distinct() :
+#        for ccb.concept_from in CCb.objects.using(db).filter(concept_to__id = concept_to__id):
+        concept = Concept.objects.using(db).get(id=concept_to_id)
         for concept_from, concept_to in combinations(concept.narrowers.all(), 2):
-            CCbb.objects.get_or_create(concept_from=concept_from, concept_parent=concept, concept_to=concept_to)
+            CCbb.objects.using(db).get_or_create(concept_from=concept_from, concept_parent=concept, concept_to=concept_to)
 
-def generate_CCbc():
-    for concept_from_id in CCb.objects.values('concept_from').distinct() :
-#        for ccb.concept_from in CCb.objects.filter(concept_to__id = concept_to__id):
-        concept = Concept.objects.get(id=concept_from_id)
+def generate_CCbc(db='default'):
+    for concept_from_id in CCb.objects.using(db).values('concept_from').distinct() :
+#        for ccb.concept_from in CCb.objects.using(db).filter(concept_to__id = concept_to__id):
+        concept = Concept.objects.using(db).get(id=concept_from_id)
         for concept_from, concept_to in combinations(concept.broaders.all(), 2):
-            CCbc.objects.get_or_create(concept_from=concept_from, concept_child=concept, concept_to=concept_to)
+            CCbc.objects.using(db).get_or_create(concept_from=concept_from, concept_child=concept, concept_to=concept_to)
 
-def generate_AAba():
-    for ccb in CCb.objects.exclude(concept_from__not_in_dataset=True, concept_to__not_in_dataset=True):
+def generate_AAba(db='default'):
+    for ccb in CCb.objects.using(db).exclude(concept_from__not_in_dataset=True, concept_to__not_in_dataset=True):
         for a_from in ccb.concept_from.actors.all():
             for a_to in ccb.concept_to.actors.all():
                 if a_from != a_to:
-                    AAba.objects.get_or_create(actor_from=a_from, actor_to=a_to, ccba=ccb)
+                    AAba.objects.using(db).get_or_create(actor_from=a_from, actor_to=a_to, ccba=ccb)
     #db.reset_queries()
 
-#    [AAba.objects.get_or_create(actor_from = a_from, actor_to = a_to, ccba = ccb) for a_to in list(ccb.concept_to.actors.all()) for a_from in list(ccb.concept_from.actors.all()) for ccb in list(CCb.objects.exclude(concept_from__not_in_dataset = True, concept_to__not_in_dataset = True)) if a_from != a_to]
+#    [AAba.objects.using(db).get_or_create(actor_from = a_from, actor_to = a_to, ccba = ccb) for a_to in list(ccb.concept_to.actors.all()) for a_from in list(ccb.concept_from.actors.all()) for ccb in list(CCb.objects.using(db).exclude(concept_from__not_in_dataset = True, concept_to__not_in_dataset = True)) if a_from != a_to]
 
-##[[[AAba.objects.get_or_create(actor_from = a_from, actor_to = a_to, ccba = ccb) for a_to in ccb.concept_to.actors.all()] for a_from in ccb.concept_from.actors.all()] for ccb in CCb.objects.exclude(concept_from__not_in_dataset = True, concept_to__not_in_dataset = True) if a_from != a_to]
+##[[[AAba.objects.using(db).get_or_create(actor_from = a_from, actor_to = a_to, ccba = ccb) for a_to in ccb.concept_to.actors.all()] for a_from in ccb.concept_from.actors.all()] for ccb in CCb.objects.using(db).exclude(concept_from__not_in_dataset = True, concept_to__not_in_dataset = True) if a_from != a_to]
 
-def generate_AAbb():
-    for ccbb in CCbb.objects.all():
-#    for ccbb in CCbb.objects.values('concept_from', 'concept_to').distinct():
-#        ccbb = CCbb.objects.get(concept_from__id=ccbb['concept_from'], concept_to__id=ccbb['concept_to'])
-#    for ccbb in [CCbb.objects.get(concept_from__id=ccbb['concept_from'], concept_to__id=ccbb['concept_to'])for ccbb in CCbb.objects.values('concept_from', 'concept_to').distinct()]:
+def generate_AAbb(db='default'):
+    for ccbb in CCbb.objects.using(db).all():
+#    for ccbb in CCbb.objects.using(db).values('concept_from', 'concept_to').distinct():
+#        ccbb = CCbb.objects.using(db).get(concept_from__id=ccbb['concept_from'], concept_to__id=ccbb['concept_to'])
+#    for ccbb in [CCbb.objects.using(db).get(concept_from__id=ccbb['concept_from'], concept_to__id=ccbb['concept_to'])for ccbb in CCbb.objects.using(db).values('concept_from', 'concept_to').distinct()]:
         for a_from in ccbb.concept_from.actors.all():
-#        for a_from in Concept.objects.get(id = ccbb['concept_from']).actors.all():
+#        for a_from in Concept.objects.using(db).get(id = ccbb['concept_from']).actors.all():
             for a_to in ccbb.concept_to.actors.all():
-#            for a_to in Concept.objects.get(id = ccbb['concept_to']).actors.all():
+#            for a_to in Concept.objects.using(db).get(id = ccbb['concept_to']).actors.all():
                 if a_from != a_to:
-#                    for ccbbe in CCbb.objects.filter('concept_from', 'concept_to')
-                    AAbb.objects.get_or_create(actor_from=a_from, actor_to=a_to, ccbb=ccbb)
+#                    for ccbbe in CCbb.objects.using(db).filter('concept_from', 'concept_to')
+                    AAbb.objects.using(db).get_or_create(actor_from=a_from, actor_to=a_to, ccbb=ccbb)
     #db.reset_queries()
 
-def generate_AAbc():
-    for ccbc in CCbc.objects.all():
+def generate_AAbc(db='default'):
+    for ccbc in CCbc.objects.using(db).all():
         for a_from in ccbc.concept_from.actors.all():
             for a_to in ccbc.concept_to.actors.all():
                 if a_from != a_to:
-                    AAbc.objects.get_or_create(actor_from=a_from, actor_to=a_to, ccbc=ccbc)
+                    AAbc.objects.using(db).get_or_create(actor_from=a_from, actor_to=a_to, ccbc=ccbc)
     #db.reset_queries()
 
 ############
 
 #def generate_AAc_weighted_from_Aconcept():
-#    for concept in Concept.objects.all():
+#    for concept in Concept.objects.using(db).all():
 #        #db.reset_queries()
 #        for a1, a2 in combinations(concept.actors.all(), 2):
-#            aac1 = AAcw.objects.filter(actor_from = a1, actor_to = a2)
-#            aac2 = AAcw.objects.filter(actor_from = a2, actor_to = a1)
+#            aac1 = AAcw.objects.using(db).filter(actor_from = a1, actor_to = a2)
+#            aac2 = AAcw.objects.using(db).filter(actor_from = a2, actor_to = a1)
 #            if aac1:
 #                aac1[0].weight += 1
 #            elif aac2:
 #                aac2[0].weight += 1
-#            AAcw.objects.get_or_create(actor_from = a1, actor_to = a2, weight=1)
+#            AAcw.objects.using(db).get_or_create(actor_from = a1, actor_to = a2, weight=1)
 
-def generate_AAc_weighted_from_Aconcept():
-    for a1, a2 in combinations(Actor.objects.all(), 2):
+def generate_AAc_weighted_from_Aconcept(db='default'):
+    for a1, a2 in combinations(Actor.objects.using(db).all(), 2):
         w = len(set(a1.concepts.all()).intersection(set(a2.concepts.all())))
         if w:
-            AAcw.objects.create(actor_from=a1, actor_to=a2, weight=w)
+            AAcw.objects.using(db).create(actor_from=a1, actor_to=a2, weight=w)
 
-def generate_AAbc_weighted():
-##    ccbc = list(CCbc.objects.values('concept_from', 'concept_to').distinct())
-#    ccbcs = list(CCbc.objects.values('concept_from', 'concept_to','ccbc')
+def generate_AAbc_weighted(db='default'):
+##    ccbc = list(CCbc.objects.using(db).values('concept_from', 'concept_to').distinct())
+#    ccbcs = list(CCbc.objects.using(db).values('concept_from', 'concept_to','ccbc')
 #    [ccbcs.remove({'concept_to':ccbbe['concept_from'], 'concept_from': ccbbe['concept_to'],'ccbc':ccbbe['ccbc']}) for ccbbe in ccbcs if {'concept_to':ccbbe['concept_from'], 'concept_from': ccbbe['concept_to'],'ccbc':ccbbe['ccbc']} in ccbcs]
-##    ccbcs = list(CCbc.objects.all())
-##    [ccbcs.remove(CCbc.objects.get(concept_to = ccbbe.concept_from, concept_from = ccbbe.concept_to)) for ccbbe in ccbcs if CCbc.objects.filter(concept_to = ccbbe.concept_from, concept_from = ccbbe.concept_to)]
-#    [ccbbe.delete() for ccbbe in CCbc.objects.all() if CCbc.objects.filter(actor_from = ccbbe.actor_to, actor_to = ccbbe.actor_from, ccbc =ccbbe.ccbc)]
-    for ccbc in CCbc.objects.all():
-        if CCbc.objects.filter(concept_from=ccbc.concept_to, concept_to=ccbc.concept_from, concept_child=ccbc.concept_child):
+##    ccbcs = list(CCbc.objects.using(db).all())
+##    [ccbcs.remove(CCbc.objects.using(db).get(concept_to = ccbbe.concept_from, concept_from = ccbbe.concept_to)) for ccbbe in ccbcs if CCbc.objects.using(db).filter(concept_to = ccbbe.concept_from, concept_from = ccbbe.concept_to)]
+#    [ccbbe.delete() for ccbbe in CCbc.objects.using(db).all() if CCbc.objects.using(db).filter(actor_from = ccbbe.actor_to, actor_to = ccbbe.actor_from, ccbc =ccbbe.ccbc)]
+    for ccbc in CCbc.objects.using(db).all():
+        if CCbc.objects.using(db).filter(concept_from=ccbc.concept_to, concept_to=ccbc.concept_from, concept_child=ccbc.concept_child):
             ccbc.delete()
         else:
             for a_from in ccbc.concept_from.actors.all():
                 for a_to in ccbc.concept_to.actors.all():
                     if a_from != a_to:
-                        a = AAbcw.objects.filter(actor_from=a_from, actor_to=a_to)
-                        b = AAbcw.objects.filter(actor_from=a_to, actor_to=a_from)
+                        a = AAbcw.objects.using(db).filter(actor_from=a_from, actor_to=a_to)
+                        b = AAbcw.objects.using(db).filter(actor_from=a_to, actor_to=a_from)
                         if a:
                             a[0].weight += 0.5
                             a[0].save()
@@ -543,22 +543,22 @@ def generate_AAbc_weighted():
                             b[0].weight += 0.5
                             b[0].save()
                         else:
-                            AAbcw.objects.create(actor_from=a_from, actor_to=a_to, weight=0.5)
+                            AAbcw.objects.using(db).create(actor_from=a_from, actor_to=a_to, weight=0.5)
 
-def generate_AAbbc_weighted():
-    [AAbbcw.objects.create(actor_from=aabcw.actor_from, actor_to=aabcw.actor_to, weight=aabcw.weight) for aabcw in AAbcw.objects.all()]
-##    ccbbs = list(CCbb.objects.all())
-#    ccbbs = list(CCbb.objects.values('concept_from', 'concept_to','ccbb')
+def generate_AAbbc_weighted(db='default'):
+    [AAbbcw.objects.using(db).create(actor_from=aabcw.actor_from, actor_to=aabcw.actor_to, weight=aabcw.weight) for aabcw in AAbcw.objects.using(db).all()]
+##    ccbbs = list(CCbb.objects.using(db).all())
+#    ccbbs = list(CCbb.objects.using(db).values('concept_from', 'concept_to','ccbb')
 #    [ccbbs.remove({'concept_to':ccbbe['concept_from'], 'concept_from': ccbbe['concept_to'], 'ccbb': ccbbe['ccbb']}) for ccbbe in ccbbs if {'concept_to':ccbbe['concept_from'], 'concept_from': ccbbe['concept_to'], 'ccbb': ccbbe['ccbb']} in ccbbs]
-    for ccbb in CCbb.objects.all():
-        if CCbb.objects.filter(concept_from=ccbb.concept_to, concept_to=ccbb.concept_from, concept_parent=ccbb.concept_parent):
+    for ccbb in CCbb.objects.using(db).all():
+        if CCbb.objects.using(db).filter(concept_from=ccbb.concept_to, concept_to=ccbb.concept_from, concept_parent=ccbb.concept_parent):
             ccbb.delete()
         else:
             for a_from in ccbb.concept_from.actors.all():
                 for a_to in ccbb.concept_to.actors.all():
                     if a_from != a_to:
-                        a = AAbbcw.objects.filter(actor_from=a_from, actor_to=a_to)
-                        b = AAbbcw.objects.filter(actor_from=a_to, actor_to=a_from)
+                        a = AAbbcw.objects.using(db).filter(actor_from=a_from, actor_to=a_to)
+                        b = AAbbcw.objects.using(db).filter(actor_from=a_to, actor_to=a_from)
                         if a:
                             a[0].weight += 0.5
                             a[0].save()
@@ -566,17 +566,17 @@ def generate_AAbbc_weighted():
                             b[0].weight += 0.5
                             b[0].save()
                         else:
-                            AAbbcw.objects.create(actor_from=a_from, actor_to=a_to)
+                            AAbbcw.objects.using(db).create(actor_from=a_from, actor_to=a_to)
 
-def generate_AAbabc_weighted():
-    [AAbabcw.objects.create(actor_from=aabbcw.actor_from, actor_to=aabbcw.actor_to, weight=aabbcw.weight) for aabbcw in AAbbcw.objects.all()]
-    ccbas = CCb.objects.exclude(concept_from__not_in_dataset=True).exclude(concept_to__not_in_dataset=True)
+def generate_AAbabc_weighted(db='default'):
+    [AAbabcw.objects.using(db).create(actor_from=aabbcw.actor_from, actor_to=aabbcw.actor_to, weight=aabbcw.weight) for aabbcw in AAbbcw.objects.using(db).all()]
+    ccbas = CCb.objects.using(db).exclude(concept_from__not_in_dataset=True).exclude(concept_to__not_in_dataset=True)
     for ccbc in ccbas:
         for a_from in ccbc.concept_from.actors.all():
             for a_to in ccbc.concept_to.actors.all():
                 if a_from != a_to:
-                    a = AAbabcw.objects.filter(actor_from=a_from, actor_to=a_to)
-                    b = AAbabcw.objects.filter(actor_from=a_to, actor_to=a_from)
+                    a = AAbabcw.objects.using(db).filter(actor_from=a_from, actor_to=a_to)
+                    b = AAbabcw.objects.using(db).filter(actor_from=a_to, actor_to=a_from)
                     if a:
                         a[0].weight += 0.25
                         a[0].save()
@@ -584,4 +584,4 @@ def generate_AAbabc_weighted():
                         b[0].weight += 0.25
                         b[0].save()
                     else:
-                        AAbabcw.objects.create(actor_from=a_from, actor_to=a_to)
+                        AAbabcw.objects.using(db).create(actor_from=a_from, actor_to=a_to)
